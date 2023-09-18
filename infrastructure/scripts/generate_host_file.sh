0698dddaf65a53b970a61b365f6a3d00337d4064
AnotherUser969 <132815401+AnotherUser969@users.noreply.github.com>
2023-09-18 22:29:19 +1000

    Update generate_host_file.sh

--- infrastructure/scripts/generate_host_file.sh
+++ infrastructure/scripts/generate_host_file.sh
@@ -1,6 +1,6 @@
 echo [application_servers] > hosts
-terraform output -json | jq -r '.app_servers_ips.value.internal[][]' >> hosts
+terraform -chdir="./infrastructure/terraform" output -json | jq -r '.app_servers_ips.value.internal[][]' >> hosts
 echo [load_balancer] >> hosts
-terraform output -json | jq -r '.load_balancer_ips.value.internal[][]' >> hosts
+terraform -chdir="./infrastructure/terraform" output -json | jq -r '.load_balancer_ips.value.internal[][]' >> hosts
 echo [monitoring] >> hosts
-terraform output -json | jq -r '.monitoring_ips.value.internal[][]' >> hosts
+terraform -chdir="./infrastructure/terraform" output -json | jq -r '.monitoring_ips.value.internal[][]' >> hosts
